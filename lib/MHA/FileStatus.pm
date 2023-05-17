038352c08ec0845ff12bd27b3a13a06d8751bfbf
slowtech <631369960@qq.com>
2023-05-17 10:21:11 +0800

    给 FileStatus.pm 添加注释

--- lib/MHA/FileStatus.pm
+++ lib/MHA/FileStatus.pm
@@ -61,9 +61,9 @@ sub set_master_host($$) {
 # utility function. getting basename from conf file
 sub get_basename($) {
   my $self     = shift;
-  my $basename = basename( $self->{conffile} );
-  $basename =~ s/(.*)\..*?$/$1/;
-  if ( $basename eq '' ) {
+  my $basename = basename( $self->{conffile} ); # 使用 basename 函数从 $self->{conffile} 中提取文件名（不包括路径）
+  $basename =~ s/(.*)\..*?$/$1/; # 使用正则表达式将文件名中的扩展名部分去除，只保留文件名的部分。
+  if ( $basename eq '' ) { # 如果去除扩展名后的文件名为空字符串，表示原始的文件名中没有扩展名部分，则将完整的文件名赋值给 $basename。
     $basename = $self->{conffile};
   }
   return $basename;
@@ -75,14 +75,14 @@ sub update_status {
   my $file          = $self->{status_file};
   my $master_host   = $self->{master_host};
   my $out;
-  if ( -f $file ) {
+  if ( -f $file ) { # 如果文件存在，则以读写模式打开文件，并对文件进行加锁、截断和重定位到文件开头。
     open( $out, "+<", $file ) or croak "$!:$file";
     flock( $out, 2 );
     truncate( $out, 0 );
     seek( $out, 0, 0 );
   }
   else {
-    open( $out, ">", $file ) or croak "$!:$file";
+    open( $out, ">", $file ) or croak "$!:$file"; # 如果文件不存在，则以写模式创建文件。
   }
   print $out "$$\t$status_string";
   print $out "\tmaster:$master_host" if ($master_host);
@@ -93,10 +93,10 @@ sub read_status {
   my $self = shift;
   my $file = $self->{status_file};
   open( my $in, "+<", $file ) or croak "$!:$file";
-  flock( $in, 2 );
-  my $line          = readline($in);
-  my @values        = split( /\t/, $line );
-  my $pid           = $values[0];
+  flock( $in, 2 ); # 以读写模式打开指定的文件，并对其进行排它性锁定，以便其他进程无法同时对文件进行写入操作。
+  my $line          = readline($in); # 读取文件的一行内容
+  my @values        = split( /\t/, $line ); # 将该行内容按制表符进行分割
+  my $pid           = $values[0]; # 分别存储到三个变量中：$pid、$status_string 和 $master_info
   my $status_string = $values[1];
   my $master_info;
 
@@ -117,7 +117,7 @@ sub update_status_time {
   }
   else {
     my $ftime = time;
-    utime $ftime, $ftime, $file;
+    utime $ftime, $ftime, $file; # 将指定文件的访问时间和修改时间设置为给定的时间戳值 $ftime。这样可以手动更新文件的时间戳，以便反映文件的最新访问和修改时间。
   }
 }
 
